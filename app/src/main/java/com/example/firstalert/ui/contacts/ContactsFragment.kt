978f30b166dd957a48921e99c22794c9febc97d0
liuruoyuan1997@gmail.com <liuruoyuan1997@gmail.com>
2020-01-03 19:56:22 -0500

    Adding missing fragments and ViewModel

---
+++ app/src/main/java/com/example/firstalert/ui/contacts/ContactsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.firstalert.ui.contacts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.firstalert.R
+
+class ContactsFragment : Fragment() {
+
+    private lateinit var contactsViewModel: ContactsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        contactsViewModel =
+            ViewModelProviders.of(this).get(ContactsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return root
+    }
+}
